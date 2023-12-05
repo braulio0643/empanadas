2e5004820874e76ed4505924a3c4320b4553eabe
Braulio Solari <brauliosolari@gmail.com>
2023-12-05 15:57:56 -0300

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import ItemListContainer from './components/ItemListContainer'
+
+const App = () => {
+
+  return (
+      <div>
+        <ItemListContainer/>
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ItemList from './ItemList'
+
+const ItemListContainer = () => {
+    return (
+        <div>ItemListContainer</div>
+    )
+}
+
+export default ItemListContainer
